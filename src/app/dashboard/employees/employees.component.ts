d0500804576af79f648301bd0389ecef2a8b5f5c
OSIRISDMN\NMayekiso <ndimphiwem@osiristrading.com>
2021-05-24 17:25:06 +0200

    feat (employees): Added employee data to the employees component and added a seach feature (live search) with debounce

--- src/app/dashboard/employees/employees.component.ts
+++ src/app/dashboard/employees/employees.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { EmployeesService } from './employees.service';
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  employees: any;
+  filteredEmployees: any;
+  searchField = new FormControl('');
+  searching = false;
+
+  constructor(readonly employeeService: EmployeesService) { }
 
   ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe(employees => {
+      this.employees = employees;
+      this.filteredEmployees = employees;
+      console.log('employees', employees);
+    });
+
+    this.searchField.valueChanges.pipe(
+      tap(() => this.searching = true),
+      debounceTime(300),
+      distinctUntilChanged(),
+    ).subscribe(searchString => {
+      this.filteredEmployees = searchString.length > 1 ? this.findEmployees(searchString) : this.employees;
+      this.searching = false;
+    });
+  }
+
+  findEmployees(searchString: string) {
+    const searchResults = this.employees.filter((employee: any) => {
+      return employee?.name?.first.toLowerCase().includes(searchString.toLowerCase())
+            || employee?.name?.last.toLowerCase().includes(searchString.toLowerCase());
+    });
+    return searchResults;
+  }
+
+  clearSearchResults() {
+    this.searchField.setValue('');
+    this.filteredEmployees = this.employees;
   }
 
 }
